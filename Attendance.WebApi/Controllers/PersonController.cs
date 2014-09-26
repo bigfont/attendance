36e873b6a33a8ee73d60844f2ea7407db759febe
Shaun Luttin <shaunluttin@gmail.com>
2014-09-26 14:59:27 -0700

    Add a connections.config.

--- Attendance.WebApi/Controllers/PersonController.cs
+++ Attendance.WebApi/Controllers/PersonController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Attendance.WebApi.Models;
+using Attendance.DataAccess.Models;
 
 namespace Attendance.WebApi.Controllers
 {
