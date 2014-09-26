74a8a4a995a8dc926e3e3709bffab36c4d09cdbd
Shaun Luttin <shaunluttin@gmail.com>
2014-09-26 14:06:22 -0700

    Add two actions.

--- Attendance.WebApi/Controllers/PersonController.cs
+++ Attendance.WebApi/Controllers/PersonController.cs
@@ -4,10 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Attendance.WebApi.Models;
 
 namespace Attendance.WebApi.Controllers
 {
     public class PersonController : ApiController
     {
+        private Person[] persons = new Person[] { 
+        
+            new Person() { Id = 01, FirstName = "Anjali", LastName = "Moreau" }, 
+            new Person() { Id = 02, FirstName = "Noah", LastName = "Jacobson" }        
+        };
+
+        public IEnumerable<Person> GetAllPersons()
+        {
+            return persons;
+        }
+
+        public IHttpActionResult GetPerson(int id)
+        {
+            var person = persons.FirstOrDefault(p => p.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
     }
 }
