71f492ef78077916a4c2ff508e54f4cb5f3b3951
Shaun Luttin <shaunluttin@gmail.com>
2014-10-03 17:19:38 -0700

    Use personDTOs.

--- Attendance.WebApi/Controllers/PersonController.cs
+++ Attendance.WebApi/Controllers/PersonController.cs
@@ -6,16 +6,22 @@ using System.Net.Http;
 using System.Web.Http;
 using Attendance.DataAccess.Models;
 using Attendance.DataAccess.DAL;
+using Attendance.WebApi.Models;
 
 namespace Attendance.WebApi.Controllers
 {
     public class PersonController : ApiController
     {
-        public IEnumerable<Person> GetAllPersons()
+        public IEnumerable<PersonDTO> GetAllPersons()
         {
             AttendanceContext db = new AttendanceContext();
             IEnumerable<Person> personsDb = db.Persons;
-            return personsDb;
+            IEnumerable<PersonDTO> personsDTO = db.Persons.Select(p => new PersonDTO { 
+                Id = p.Id,
+                FirstName = p.FirstName, 
+                LastName = p.LastName
+            });
+            return personsDTO;
         }
 
         public IHttpActionResult GetPerson(int id)
@@ -29,12 +35,24 @@ namespace Attendance.WebApi.Controllers
             return Ok(person);
         }
 
-        // $body = @{ FirstName = "Shaun" } | ConvertTo-JSON
+        // $body = @{ FirstName = "Shaun"; LastName = "Luttin" } | ConvertTo-JSON
         // Invoke-RestMethod http://localhost/Attendance.WebApi/api/person -Method POST -ContentType "application/json" -Body $body -Debug
-        public HttpResponseMessage PostPerson(Person person)
+        public HttpResponseMessage PostPerson(PersonDTO personDTO)
         {
             // see also
             // http://www.asp.net/web-api/overview/creating-web-apis/using-web-api-1-with-entity-framework-5/using-web-api-with-entity-framework,-part-6
+
+            var person = new Person() { 
+                Id = personDTO.Id,
+                FirstName = personDTO.FirstName, 
+                LastName = personDTO.LastName
+            };            
+
+            using (AttendanceContext db = new AttendanceContext())
+            {
+                db.Persons.Add(person);
+                db.SaveChanges();
+            }
 
             HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, person);
             return response;
