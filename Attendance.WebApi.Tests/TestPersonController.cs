87b481dabeb368cc11268f2a2b42e6ebe6d2c77e
Shaun Luttin <shaun@bigfont.ca>
2014-10-14 15:23:11 -0700

    Add unit test.

---
+++ Attendance.WebApi.Tests/TestPersonController.cs
@@ -0,0 +1,28 @@
+﻿using Attendance.WebApi.Controllers;
+using Attendance.WebApi.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Attendance.WebApi.Tests
+{
+    [TestClass]
+    public class TestPersonController
+    {
+        [TestMethod]
+        public void TestPostPerson()
+        {
+            System.Diagnostics.Debugger.Break();
+
+            PersonDTO dto = new PersonDTO()
+            {
+                FirstName = "Test First Name",
+                LastName = "Test Last Name"
+            };
+
+            var controller = new PersonController();
+
+
+            //var response = controller.PostPerson(dto);
+        }
+    }
+}
