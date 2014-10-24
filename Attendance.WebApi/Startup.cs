fc54b2e85f48f2bb63287f67082ead121505910d
Shaun Luttin <shaunluttin@gmail.com>
2014-10-24 14:00:15 -0700

    Install microsoft.owin.

--- Attendance.WebApi/Startup.cs
+++ Attendance.WebApi/Startup.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.Owin;
 using Owin;
+
+[assembly:OwinStartup(typeof(Attendance.WebApi.Startup))]
 
 namespace Attendance.WebApi
 {
